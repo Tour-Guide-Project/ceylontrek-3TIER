a476420478c7e8df329bcfb670844089b22d6e99
KAVINDYADEVINDI <63293156+KAVINDYADEVINDI@users.noreply.github.com>
2020-11-06 23:10:09 +0530

    k4

---
+++ view/SSR_ViewMorePage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Search Results Page</title>
+	<link rel="stylesheet" type="text/css" href="../resources/css/SSR_ViewMorePage.css">
+	<link rel="stylesheet" href="../resources/css/top_bar.css">
+    <link rel="stylesheet" href="../resources/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body style="background-image: url('../resources/img/2.jpg'); background-size:cover;background-position: center center;background-attachment: fixed; background-repeat:no-repeat;">
+
+	<?php include('../view/top_bar.php');?>
+
+	<div class="con">
+		<h1 class="ttl"><b>Hikkaduwa</b></h1>
+		<div class="image">
+			<img src="../resources/img/1.jpg" alt="Paris" style="width:60%;">
+		</div>
+
+		<div>
+			<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka.</p>
+
+			<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka.</p>
+
+			<p class="paragraph">Hikkaduwa is one of the most beautiful place in Sri Lanka. Hikkaduwa is one of the most beautiful place in Sri Lanka.</p>
+		</div>
+	</div>
+
+<?php include('../view/footer.php');?>
+
+</body>
+</html>
