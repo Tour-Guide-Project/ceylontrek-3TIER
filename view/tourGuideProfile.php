c780079e1dae0c6eb05ebab00a4e1fb29334b1a7
KAVINDYADEVINDI <63293156+KAVINDYADEVINDI@users.noreply.github.com>
2020-11-07 01:21:46 +0530

    k4

---
+++ view/tourGuideProfile.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tour Guide Profile</title>
+    <link rel="stylesheet" href="../resources/css/homepage.css">
+    <link rel="stylesheet" href="../resources/css/tourGuideProfile.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../resources/js/guideProfile.js"></script> 
+</head>
+
+<body style="background-color:whitesmoke">
+    <div class="container">
+
+
+    <?php include('../view/new_top_bar.php'); ?>
+
+
+        <div class="sidenav">
+            <h2 style="margin-left: 20px; margin-bottom:-20px;">Reviews</h2>
+            
+            
+
+          <div class="wrapper relative">
+          
+          <span class="client-name">Ricky Ponting</span>
+          <span class="client-country">Australia</span>
+          <div class="testimonial-content">
+          Chathura was probably the best tour guide we met in Sri Lanka. His prices were great and his service was great. will meet him soon.
+          </div>
+          <a href="url" class="viewmore">View More....</a>
+          </div>
+
+
+            <div class="wrapper relative">
+          
+            <span class="client-name">Chris Gayle</span>
+            <span class="client-country">West Indies</span>
+            <div class="testimonial-content">
+            Chathura was probably the best tour guide we met in Sri Lanka. His prices were great and his service was great. will meet him soon.
+            </div>
+            <a href="url" class="viewmore">View More....</a>
+            </div>
+
+            <div class="wrapper relative">
+          
+          <span class="client-name">Anil Kumble</span>
+          <span class="client-country">India</span>
+          <div class="testimonial-content">
+          Chathura was probably the best tour guide we met in Sri Lanka. His prices were great and his service was great. will meet him soon.
+          </div>
+          <a href="url" class="viewmore">View More....</a>
+          </div>
+
+          <div class="wrapper relative">
+          
+          <span class="client-name">Ricky Ponting</span>
+          <span class="client-country">Australia</span>
+          <div class="testimonial-content">
+          Chathura was probably the best tour guide we met in Sri Lanka. His prices were great and his service was great. will meet him soon.
+          </div>
+          <a href="url" class="viewmore">View More....</a>
+          </div>
+
+          <button class="loginbutton" style="width:200px; margin-right:60px"><span>View All Reviews</span></button>
+        
+
+
+            
+            
+            
+        </div>
+        <!-- sidenav -->
+        <div class="guideInfo">
+            
+        
+            <div class="guidewrapper ">
+            <div class="avtar"><img src="../resources/img/guide/1.jpg" alt="user-avtar" width="100%"/></div>
+            <span class="guide-name">Chathura Rathnayake</span>
+            
+            <div class="star-rating">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            
+
+            </div>
+            <div class="guide-content">
+           <ul>
+               <li>Trips Completed : 45</li>
+               <li>Years of Experience : 6</li>
+               <li>Four Star Rated Tours : 30</li>
+               <li>Fluent Languages: <ul><li>English</li>
+            <li>Spanish</li>
+        <li>Dutch</li></ul></li>
+           </ul>
+            </div>
+           
+            </div>
+            <button class="loginbutton" style="height:65px; margin-left:5px"><span>Message Guide</span></button>
+            <button class="loginbutton"style="height:65px; margin-left:0px"><span>Reserve Guide</span></button>
+</div>
+
+         <!-- end of guide info -->
+
+            <div class="info">
+    
+                    <!-- Slideshow container -->
+            <div class="slideshow-container">
+
+            <!-- Full-width images with number and caption text -->
+            <div class="mySlides fade">
+            <img src="../resources/img/guideProfile/1.jpg" style="width:640px; height:380px">
+            
+            </div>
+
+            <div class="mySlides fade">
+            
+            <img src="../resources/img/guideProfile/2.jpg" style="width:640px ;height:380px">
+            
+            </div>
+
+            <div class="mySlides fade">
+            
+            <img src="../resources/img/guideProfile/3.jpg" style="width:640px ;height:380px">
+            
+            </div>
+
+            <div class="mySlides fade">
+            
+            <img src="../resources/img/guideProfile/4.jpg" style="width:640px ;height:380px">
+            
+            </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            </div>
+            <br>
+        
+            <!-- The dots/circles -->
+            <div class="circles"> <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+            <span class="dot" onclick="currentSlide(4)"></span>
+            </div> </div>
+
+            <!-- end of slideshow-container -->
+
+            <div class="guide-bio">
+                <p><b><h4>Hello! Bonjour! Guten tag! Ayubowan!! I'm Chathura Rathnayake</h4></b></p>
+                <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <p>So choose me as your Tour Guide to travel in the Paradise Island.</p>
+                <h2 style="margin-top: 20px;  font-weight:bold;">Tour Packages offered by Chathura :</h2>
+            </div>
+
+           <!-- start of tour packages -->
+          
+                <div class="card">
+                <img src="../resources/img/guideProfile/packages/3.jpg"  style="width:640px ;height:380px">
+                <h1>Colombo in a Day</h1>
+               
+                <p>Explore the Capital of three Sri Lanka in a single day..</p>
+                <p><button>View Package >></button></p>
+                </div> 
+                        <div class="card">
+                <img src="../resources/img/guideProfile/packages/1.jpg"  style="width:640px ;height:380px">
+                <h1>Badulla, Bandarawela Tour and Hike</h1>
+               
+                <p>Get a glimpse of the best scenery and greenery Sri Lanka has to offer in three days..</p>
+                <p><button>View Package >></button></p>
+                </div> 
+
+                <div class="card">
+                <img src="../resources/img/guideProfile/packages/2.jpg" 
+                 style="width:640px ;height:380px">
+                <h1>Down South Adventure</h1>
+               
+                <p>Great Beaches, Great sea food, Great Nights..</p>
+                <p><button>View Package >></button></p>
+                </div> 
+
+
+                <!-- end of tour packages -->
+            </div>
+    <!-- end of info -->
+    <div class="dashend"> <?php include('../view/footer.php'); ?> </div>
+
+    </div>
+    <!-- container -->
+
+
+    <script>
+  showSlides(slideIndex);
+  
+</script>
+</body>
+
+</html>>
---
+++ view/tourGuideSearchResults.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tour Guide Search Results</title>
+    <link rel="stylesheet" href="../resources/css/homepage.css">
+    <link rel="stylesheet" href="../resources/css/tourGuideSearchResults.css">
+    <link rel="stylesheet" href="../resources/css/top_bar.css">
+    <link rel="stylesheet" href="../resources/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body style="background-color:#DDE2DD">
+
+    <div class="container">
+
+   
+    <?php include('../view/new_top_bar.php'); ?>
+
+
+        <div class="sidenav">
+        
+            <h3 style="margin-left: 20px; font-weight:30px; font-size:22px">Filter By</h2>
+            <label class="checkbox">Guides with Vehicles
+                    <input type="checkbox" checked="checked">
+                    <span class="checkmark"></span>
+                  </label>
+            <label class="checkbox">Round-Trip Guides
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                  </label>
+            <label class="checkbox">Per-Day Guides
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                  </label>
+            <label class="checkbox">Completed more than 100 tours
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                  </label>
+            <label class="checkbox">4 Stars Rated
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                  </label>
+            <label class="checkbox">3 Stars Rated
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                  </label>
+            <label class="checkbox">2 Stars Rated
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                  </label>
+            <label class="checkbox">1 Star Rated
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                  </label>
+            <label class="checkbox">Not Rated
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                  </label>
+        </div>
+        <!-- sidenav -->
+
+
+        <div class="sortby">
+            <h2 style="display: inline; ">Sort By</h2>
+            <button style="margin-left: 40px;">Most Trips Completed</button>
+            <button>Most Reviews</button>
+            <button>Most Stars</button>
+            <button>Least Per-Day Price</button>
+            <button>Most Recent</button>
+
+
+        </div>
+        <!-- sortby -->
+
+        <div class="tourGuide">
+
+            <!-- Start	Guide details -->
+            <div class="guide-details">
+
+                <!-- 	guide Name -->
+                <h1>Chathura Rathnayake</h1>
+
+                <span class="hint-star star">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half-o" aria-hidden="true"></i>
+            <i class="fa fa-star-o" aria-hidden="true"></i>
+          </span>
+
+
+                <!-- Guide bio -->
+                <p class="information">
+                    I'm an experienced Tour Guide who has been working in the industry for almost a decade. I have completed more than 250 local tours and more than 100 international tours. With reasonable prices and high quality service you will never regret choosing me
+                    as your travel guide and friend from Sri Lanka.
+                </p>
+
+
+
+                <!-- 		Control -->
+                <div class="control">
+
+                    <!-- Start Button buying -->
+                    <button class="btn"><span> View More</span>
+                           
+         </button>
+                    <!-- End Button buying -->
+
+                </div>
+
+            </div>
+
+            <!-- 	End	guide details   -->
+
+
+
+            <!-- 	Start guide image & Information -->
+
+            <div class="guide-image">
+
+                <img src="../resources/img/guideresult/1.jpg" alt="Omar Dsoky">
+
+            </div>
+            <!--  End guide image  -->
+
+
+        </div>
+        <!-- tourGuide1 -->
+
+        <div class="tourGuide">
+
+            <!-- Start	Guide details -->
+            <div class="guide-details">
+
+                <!-- 	guide Name -->
+                <h1>Iranga Mudalige</h1>
+
+                <span class="hint-star star">
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star-half-o" aria-hidden="true"></i>
+          <i class="fa fa-star-o" aria-hidden="true"></i>
+        </span>
+
+
+                <!-- Guide bio -->
+                <p class="information">
+                    I'm an experienced Tour Guide who has been working in the industry for almost a decade. I have completed more than 250 local tours and more than 100 international tours. With reasonable prices and high quality service you will never regret choosing me
+                    as your travel guide and friend from Sri Lanka.
+                </p>
+
+
+
+                <!-- 		Control -->
+                <div class="control">
+
+                    <!-- Start Button buying -->
+                    <button class="btn"><span> View More</span>
+                         
+       </button>
+                    <!-- End Button buying -->
+
+                </div>
+
+            </div>
+
+            <!-- 	End	guide details   -->
+
+
+
+            <!-- 	Start guide image & Information -->
+
+            <div class="guide-image">
+
+                <img src="../resources/img/guideresult/2.jpg" alt="Omar Dsoky">
+
+            </div>
+            <!--  End guide image  -->
+
+
+        </div>
+        <!-- tourGuide2 -->
+        <div class="tourGuide">
+
+            <!-- Start	Guide details -->
+            <div class="guide-details">
+
+                <!-- 	guide Name -->
+                <h1>Chathura Rathnayake</h1>
+
+                <span class="hint-star star">
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star-half-o" aria-hidden="true"></i>
+            <i class="fa fa-star-o" aria-hidden="true"></i>
+          </span>
+
+
+                <!-- Guide bio -->
+                <p class="information">
+                    I'm an experienced Tour Guide who has been working in the industry for almost a decade. I have completed more than 250 local tours and more than 100 international tours. With reasonable prices and high quality service you will never regret choosing me
+                    as your travel guide and friend from Sri Lanka.
+                </p>
+
+
+
+                <!-- 		Control -->
+                <div class="control">
+
+                    <!-- Start Button buying -->
+                    <button class="btn"><span> View More</span>
+                           
+         </button>
+                    <!-- End Button buying -->
+
+                </div>
+
+            </div>
+
+            <!-- 	End	guide details   -->
+
+
+
+            <!-- 	Start guide image & Information -->
+
+            <div class="guide-image">
+
+                <img src="../resources/img/guideresult/1.jpg" alt="Omar Dsoky">
+
+            </div>
+            <!--  End guide image  -->
+
+
+        </div>
+        <!-- tourGuide1 -->
+
+        <div class="tourGuide">
+
+            <!-- Start	Guide details -->
+            <div class="guide-details">
+
+                <!-- 	guide Name -->
+                <h1>Iranga Mudalige</h1>
+
+                <span class="hint-star star">
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star-half-o" aria-hidden="true"></i>
+          <i class="fa fa-star-o" aria-hidden="true"></i>
+        </span>
+
+
+                <!-- Guide bio -->
+                <p class="information">
+                    I'm an experienced Tour Guide who has been working in the industry for almost a decade. I have completed more than 250 local tours and more than 100 international tours. With reasonable prices and high quality service you will never regret choosing me
+                    as your travel guide and friend from Sri Lanka.
+                </p>
+
+
+
+                <!-- 		Control -->
+                <div class="control">
+
+                    <!-- Start Button buying -->
+                    <button class="btn"><span> View More</span>
+                         
+       </button>
+                    <!-- End Button buying -->
+
+                </div>
+
+            </div>
+
+            <!-- 	End	guide details   -->
+
+
+
+            <!-- 	Start guide image & Information -->
+
+            <div class="guide-image">
+
+                <img src="../resources/img/guideresult/2.jpg" alt="Omar Dsoky">
+
+            </div>
+            <!--  End guide image  -->
+
+
+        </div>
+        <!-- tourGuide2 -->
+
+        <div class="dashend"> <?php include('../view/footer.php'); ?> </div>
+
+    </div>
+    <!-- container -->
+
+</body>
+
+</html>>
