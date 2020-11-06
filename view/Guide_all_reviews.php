15ea721a3a407ec85e46b979b3a1fe98c3b3c92d
KAVINDYADEVINDI <63293156+KAVINDYADEVINDI@users.noreply.github.com>
2020-11-06 13:56:37 +0530

    k

---
+++ view/Guide_all_reviews.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset='utf-8'>
+    <title>Tour_Guide_All_Reviews</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../resources/css/GuideAllReviews.css'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../resources/css/top_bar.css">
+    <link rel="stylesheet" href="../resources/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body class="Guide_All_Reviewsbody">
+
+<?php include('../view/top_bar.php'); ?> 
+ <div class="wrapper">
+      <div class="split_left" >
+           <img src="../resources/img/reviewimg.jpg">
+           <h3>Guide name</h3>
+      </div><!--split_left--> 
+
+    
+        
+      <div class="box rating">
+                
+                    <table>
+
+                        <tr>
+                           <div class="star 4">
+                        <td>
+                             <div class="star-container">
+                                 <span class="fa fa-star checked"></span>
+                                 <span class="fa fa-star checked"></span>
+                                 <span class="fa fa-star checked"></span>
+                                 <span class="fa fa-star checked"></span>
+                             </div><!--star-container-->
+                       </td>
+
+                       <td>
+                             <div class="bar-container">
+                                <div class="bar-4">
+    
+                                </div>
+                             </div><!--bar-container-->
+                        </td>
+                </div><!--star 4-->
+                        
+                     </tr>
+
+                     <tr>
+                    <div class="star 3">
+                         <td>
+                              <div class="star-container">
+                                  <span class="fa fa-star checked"></span>
+                                  <span class="fa fa-star checked"></span>
+                                  <span class="fa fa-star checked"></span>
+                              </div><!--star-container-->
+                        </td>
+ 
+                        <td>
+                              <div class="bar-container">
+                                 <div class="bar-3">
+     
+                                 </div>
+                              </div><!--bar-container-->
+                         </td>
+                     </div><!--star 3-->
+                         
+                      </tr>
+
+                      <tr>
+                            <div class="star 2">
+                         <td>
+                              <div class="star-container">
+                                  <span class="fa fa-star checked"></span>
+                                  <span class="fa fa-star checked"></span>
+                              </div><!--star-container-->
+                        </td>
+ 
+                        <td>
+                              <div class="bar-container">
+                                 <div class="bar-2">
+     
+                                 </div>
+                              </div><!--bar-container-->
+                         </td>
+                 </div><!--star 2-->
+                         
+                      </tr>
+
+                      <tr>
+                            <div class="star 1">
+                         <td>
+                              <div class="star-container">
+                                  <span class="fa fa-star checked"></span>
+                                  
+                              </div><!--star-container-->
+                        </td>
+ 
+                        <td>
+                              <div class="bar-container">
+                                 <div class="bar-1">
+     
+                                 </div>
+                              </div><!--bar-container-->
+                         </td>
+                 </div><!--star 1-->
+                         
+                      </tr>
+
+
+
+            </table>    
+            </div><!--star_rating-->     
+                   
+    
+
+       <div class="reviews">
+            <div class="box review">
+                  <h5>Kavindya Devindi</h5>
+                    <p>
+                          The trip was amazing, a once in a lifetime opportunity and trip. My sister and I spent 16 days in Italy for her high school graduation trip, and we had an unparalleled experience. We went from Venice to Florence/Tuscany to Cinque Terre to Sorrento to Rome. Everything in each city and in between was incredible. 
+                    </p>
+                    
+              </div><!--box review-->
+  
+              <div class="box review">
+                      <h5>Dinendra Bandara</h5>
+                      <p>
+                            The trip was amazing, a once in a lifetime opportunity and trip. My sister and I spent 16 days in Italy for her high school graduation trip, and we had an unparalleled experience. We went from Venice to Florence/Tuscany to Cinque Terre to Sorrento to Rome. Everything in each city and in between was incredible. 
+                      </p>
+                      
+              </div><!--box review-->
+  
+              <div class="box review">
+                      <h5>Sulakshani Theja</h5>
+                      <p>
+                            The trip was amazing, a once in a lifetime opportunity and trip. My sister and I spent 16 days in Italy for her high school graduation trip, and we had an unparalleled experience. We went from Venice to Florence/Tuscany to Cinque Terre to Sorrento to Rome. Everything in each city and in between was incredible. 
+                      </p>
+                      
+              </div><!--box review-->
+              
+  
+              <div class="box review">
+                      <h5>Piyumi Dulanjali</h5>
+                      <p>
+                            The trip was amazing, a once in a lifetime opportunity and trip. My sister and I spent 16 days in Italy for her high school graduation trip, and we had an unparalleled experience. We went from Venice to Florence/Tuscany to Cinque Terre to Sorrento to Rome. Everything in each city and in between was incredible. 
+                      </p>
+                      
+                  </div><!--box review-->
+                 
+       </div><!--reviews-->
+        <div class="reviewend"><?php include('../view/footer.php'); ?> </div>
+      </div><!--wrapper-->
+      
+</body>
+  
+</html>
---
+++ view/Guide_search_page.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Search Tour Guide</title>
+  
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="../resources/css/top_bar.css">
+   <link rel="stylesheet" href="../resources/css/footer.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../resources/css/guide_searchpage.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+</head>
+<body>
+<?php include('../view/top_bar.php'); ?> 
+   <div class="header" >
+
+   
+      <form>
+      	<div class="container">
+      	<div class="form-box">
+            <h1>Search Tour Guide</h1>
+      		
+   				<input class="search destinations" type="text" name="destinations" id="" placeholder="Destinations">
+   			
+
+   				<input class="search arrivaldate" type="text" name="arrivaldate" id="" placeholder="Arrival_Date(DD/MM/YY)">
+   			
+
+   				<input class="search departuredate" type="text" name="departuredate" id="" placeholder="Departure_Date(DD/MM/YY)">
+   			
+   			
+   				<input class="search numoftourist" type="text" name="numoftourist" id="" placeholder="Number_Of_Tourist">
+
+   			
+   				<button class="Search-btn" type="button">SEARCH</button>
+
+               <br><br>
+
+               <div class="check-box">
+      
+                  <input type="checkbox" name="" id="Guide with vehicles">
+                  <label for="Guide with vehicles">Guide with vehicles</label>
+
+                  <input type="checkbox" name="" id="Round Trip Guides">
+                  <label for="Round Trip Guides">Round Trip Guides</label>
+
+                  <input type="checkbox" name="" id="Per-Day Guides">
+                  <label for="Per-Day Guides">Per-Day Guides</label>
+
+               </div><!--check-box-->
+                 <br><br><br><br>
+                 <button class="seeall-button">SEE ALL Guides</button>
+      	</div><!--form-box-->
+
+
+      </form>
+</div>
+   </div><!--header-->
+   <?php include('../view/footer.php'); ?>
+</body>
+</html>
