67e9f8f0582641f0c44fbe335e13be3a2023b440
KAVINDYADEVINDI <63293156+KAVINDYADEVINDI@users.noreply.github.com>
2020-11-06 16:42:33 +0530

    k

---
+++ view/homepage.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ceylon Trek - Home</title>
+    <link rel="stylesheet" href="../resources/css/homepage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <img src="img/home/fish1.jpg" alt=""> -->
+</head>
+
+<body>
+    <div class="container">
+
+
+        <nav class="navbar">
+            <ul>
+                <!-- <li><a href="#home">Home</a></li> -->
+                <li style=" padding: 0px; width:200px; height: 75px; margin-bottom: 0px; margin-right: 40px;">
+                    <a href="#"><img src="../resources/img/home/logo2.png" alt=""></a>
+                </li>
+                <li style="margin-top: 30px;"><a href="#about">Tour Guides</a></li>
+                <li style="margin-top: 30px;"><a href="#service">Tour Packages</a></li>
+                <li style="margin-top: 30px;"><a href="#contact">Request a Tour</a></li>
+                <li style="margin-top: 30px;"><a href="#contact">Smart Search</a></li>
+                <li style="margin-top: 30px;"><a href="#contact">Travel Calendar</a></li>
+            </ul>
+
+            <button class="loginbutton"><span>Login</span></button>
+            <button class="loginbutton" id="signup"><span>SignUp</span></button>
+        </nav>
+        <section id="home">
+            <h1>Ceylon Trek</h1>
+            <p class="lead">Travel Sri Lanka</p>
+        </section>
+        <section id="tourGuide">
+
+            <h1>Tour Guides</h1>
+            <p class="lead" style="float: left;">Sri Lankans are world-renowned for their hospitality. So are our Tour Guides.<br> Ceylon Trek offers the best and the most experienced Tour Guides available in Sri Lanka. <br>For your safety, we choose only the registered Tour Guides of the
+                SriLankan Tourism Beaurae.<br> Reserve our Guides to get the most out of Sri Lanka. </p>
+            <button class="loginbutton" style="width: 250px; margin-left: 350px; margin-top: 10px; font-size: 1.25rem; height: 70px; "><span>Explore Tour Guides</span></button>
+
+
+
+
+        </section>
+        <section id="tourPackage">
+            <h1>Tour Packages</h1>
+            <p class="lead" style="float: left;">Sri Lanka is a small island packed with a lot of places to travel.<br> Ceylon Trek provides a variety of Tour Packages which will get where you want.<br> Choose from a variety of tour packages from One day Tours to Round trip Tours offered
+                by our Tour Guides to get the most of this little island packed with a lot of surprises. </p>
+            <button class="loginbutton" style="width: 300px; margin-left: 650px; margin-top: 10px; font-size: 1.25rem; height: 70px; "><span>Explore Tour Packages</span></button>
+
+        </section>
+        <section id="requestTour">
+            <h1>Request a Tour</h1>
+            <p class="lead" style="float: left;">Not happy with the Tour Packages offered?<br> Ceylon Trek got you covered!<br> Simply post a request for a Tour and let all our Tour Guides know how you want to<br> spend your holiday in Paradise and wait for them to come up with the best
+                itinerary for you. </p>
+            <button class="loginbutton" style="width: 250px; margin-left: 350px; margin-top: 10px; font-size: 1.25rem; height: 70px; "><span>Request a Tour</span></button>
+
+        </section>
+        <section id="smartSearch">
+            <h1>Smart Search</h1>
+            <p class="lead" style="float: left;">Don't know where to travel?<br> We at Ceylon Trek are ready to help.<br> Simply choose your favourite activities and hobbies<br> and our Smart Search will suggest where exactly you belong in paradise !!! </p>
+            <button class="loginbutton" style="width: 250px; margin-left: 650px; margin-top: 10px; font-size: 1.25rem; height: 70px; "><span>Smart Search</span></button>
+
+        </section>
+        <section id="travelCalendar">
+            <h1>Travel Calendar</h1>
+            <p class="lead" style="float: left;">Paradise isn't Paradise if it isn't happening!!<br> Use our travel calendar to choose the right days to travel to Sri Lanka to get the most <br>out of your visit. Because being a multi-ethnic, multi-cultural country, <br>Sri Lanka has a festival
+                for everyone! </p>
+            <button class="loginbutton" style="width: 250px; margin-left: 350px; margin-top: 10px; font-size: 1.25rem; height: 70px; "><span>Travel Calendar</span></button>
+
+        </section>
+        <footer class="footer-distributed">
+
+<div class="footer-left">
+
+<a href="#"><img src="../resources/img/home/logo2.png" alt=""></a>
+
+    <p class="footer-links">
+        <a href="#" class="link-1">Home</a>
+        
+        <a href="#">Tour Guides</a>
+    
+        <a href="#">Tour Packages</a>
+    
+        <a href="#">Request a Tour</a>
+        
+        <a href="#">Smart Search</a>
+        
+        <a href="#">Travel Calendar</a>
+    </p>
+
+    <p class="footer-company-name">Ceylon Trek © 2020</p>
+</div>
+
+<div class="footer-center">
+
+    <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>Reid Avenue</span> Colombo 7, Sri Lanka</p>
+    </div>
+
+    <div>
+        <i class="fa fa-phone"></i>
+        <p>+9411222222</p>
+    </div>
+
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:support@company.com">support@ceylontrek.com</a></p>
+    </div>
+
+</div>
+
+<div class="footer-right">
+
+    <p class="footer-company-about">
+        <span>About Ceylon Trek</span>
+        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+        <p ><a style="color:white;" href="#">View More >></a></p>
+    </p>
+
+    <div class="footer-icons">
+
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+
+    </div>
+
+</div>
+
+</footer>
+
+    </div>
+
+    
+
+</body>
+
+</html>
