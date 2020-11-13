26fe931c47e4c13c90e9d06f351e5ec798d2f946
dinendra007 <dinendradb@gmail.com>
2020-11-13 11:58:52 +0530

    footer linked

--- view/homepage.php
+++ view/homepage.php
@@ -72,68 +72,7 @@
             <button class="loginbutton" style="width: 250px; margin-left: 350px; margin-top: 10px; font-size: 1.25rem; height: 70px; "><span><a style="color:white; text-decoration:none;" href='calendar.php'>Travel Calendar</a></span></button>
 
         </section>
-        <footer class="footer-distributed">
-
-<div class="footer-left">
-
-<a href="#"><img src="../resources/img/home/logo2.png" alt=""></a>
-
-    <p class="footer-links">
-        <a href="#" class="link-1">Home</a>
-        
-        <a href="#">Tour Guides</a>
-    
-        <a href="#">Tour Packages</a>
-    
-        <a href="#">Request a Tour</a>
-        
-        <a href="#">Smart Search</a>
-        
-        <a href="#">Travel Calendar</a>
-    </p>
-
-    <p class="footer-company-name">Ceylon Trek © 2020</p>
-</div>
-
-<div class="footer-center">
-
-    <div>
-        <i class="fa fa-map-marker"></i>
-        <p><span>Reid Avenue</span> Colombo 7, Sri Lanka</p>
-    </div>
-
-    <div>
-        <i class="fa fa-phone"></i>
-        <p>+9411222222</p>
-    </div>
-
-    <div>
-        <i class="fa fa-envelope"></i>
-        <p><a href="mailto:support@company.com">support@ceylontrek.com</a></p>
-    </div>
-
-</div>
-
-<div class="footer-right">
-
-    <p class="footer-company-about">
-        <span>About Ceylon Trek</span>
-        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-        <p ><a style="color:white;" href="#">View More >></a></p>
-    </p>
-
-    <div class="footer-icons">
-
-        <a href="#"><i class="fa fa-facebook"></i></a>
-        <a href="#"><i class="fa fa-twitter"></i></a>
-        <a href="#"><i class="fa fa-linkedin"></i></a>
-        <a href="#"><i class="fa fa-instagram"></i></a>
-
-    </div>
-
-</div>
-
-</footer>
+        <div class="dashend"> <?php include('../view/footer.php'); ?> </div>
 
     </div>
 
