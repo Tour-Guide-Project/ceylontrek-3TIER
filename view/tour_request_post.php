7adc436229eba181ea565283ae5f0ee655695859
Piyumi047 <piyumidulanjali1998@gmail.com>
2020-11-19 02:06:03 +0530

    request_post

--- view/tour_request_post.php
+++ view/tour_request_post.php
@@ -1,3 +1,8 @@
+<?php session_start();
+//get the session value for varible
+$lists = $_SESSION['list'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,64 +19,51 @@
 	<?php include('../view/top_bar.php'); ?>
 	<div class="tour_request_post_box">
 			<!-- <h1>Tour Request Post </h1> -->
-			<form action="tour_request_post.php" method="post">
+			<form action="..\controller\tour_request_post_Controller.php" method="post">
 
 				<div class="text_box_search_bar">
-					<input type="text" name="" placeholder="Keyword.."></input>
-					<input type="submit" name="" value="Search"></input>
+					<input type="text" name="word" placeholder="Type Place or Date......" ></input>
+					<input type="submit" name="search" value="Search"></input>
 				</div><!-- text_box_search_bar-->	
 
 				<div class="search_bar">
 					<button type="submit" name="new_submit">Newest First</button>
 					<button type="submit" name="old_submit">Oldest First</button>
 				</div><!-- search_bar -->
-				
-				<div class="text_box">
-					<p><h2>4 days around Galle, Hambanthota and Unawatuna</h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<button type="submit"><a style="color:white; text-decoration:none;" href='full_tour_request_post_view.php'>View More &raquo;</a></button></p>
+								    
+				<div class="allposts">
+					
+				         <!--get associative array element to display-->
+				         <?php
+						 
+					      foreach ($lists as $list_element){
+						 ?>
+						 
+						 <ul>
+							<h3><?php echo $list_element['title']; ?></h3>
+							<li><i class='fa fa-calendar' aria-hidden='true'></i>Requested Date :<?php echo $list_element['requested_date']; ?></li>
+							<li><i class='fa fa-clone' aria-hidden='true'></i>NO Of Dates :<?php echo $list_element['no_of_days']; ?></li>
+							<li><i class='fa fa-map-marker' aria-hidden='true'style="margin-right:25px" ></i>Places :<?php echo $list_element['places']; ?></li>
+							<button name='viewmore'><a href='../controller/full_tour_request_post_view_controller.php?post_id=<?php echo $list_element['post_id']; ?>'>View More &raquo</a></button>
+						  </ul>
 
-				</div>
-				<div class="text_box">
-					<p><h2>4 days around Galle, Hambanthota and Unawatuna</h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<button type="submit"><a style="color:white; text-decoration:none;" href='full_tour_request_post_view.php'>View More &raquo;</a></button></p>
-					
-				</div>
-				<div class="text_box">
-					<p><h2>4 days around Galle, Hambanthota and Unawatuna</h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<button type="submit"><a style="color:white; text-decoration:none;" href='full_tour_request_post_view.php'>View More &raquo;</a></button></p>
-					
-				</div>
-				<div class="text_box">
-					<p><h2>4 days around Galle, Hambanthota and Unawatuna</h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<button type="submit"><a style="color:white; text-decoration:none;" href='full_tour_request_post_view.php'>View More &raquo;</a></button></p>
-					
+
+					    <?php
+						  }  
+					    ?>
+					  
+					  	    				    
 				</div>
 			</form>
 
 	</div><!-- tour_request_post_box -->
 
 	<div class="create_request_btn">
-		<button onclick="openForm()"> Create New Request</button>
+		<button onclick="openForm()" name="p_create"> Create New Request</button>
 	</div><!-- create_request_btn -->
 
 	<div class="form-popup" id="myForm">
-	<form action="../controller/create_new_request_controller.php" class="form-container" method="post">
+	<form action="../controller/create_request_post_controller.php" class="form-container" method="post">
 			   <h1>Create Your Tour Request</h1>
 
                <div>
@@ -96,7 +88,7 @@
 
                <label for="details"><b>Activities :</b></label>
     		   <textarea rows = "4" cols = "20" name = "activities" style="resize: vertical;height:100px;" placeholder="Enter Activites & Other details here..."></textarea>
-    		<button type="submit" class="btn">Post</button>
+    		<button type="submit" class="btn" name="submit" id="submit" >Post</button>
     		<button type="button" class="btn cancel" onclick="closeForm()">Cancel</button>
   		</form>
 	</div>
