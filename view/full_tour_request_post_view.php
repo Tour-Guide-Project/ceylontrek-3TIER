9998052ae252d845c090f7a7168823f29d87a7a7
Piyumi047 <piyumidulanjali1998@gmail.com>
2020-11-19 01:59:58 +0530

    full_post_view

--- view/full_tour_request_post_view.php
+++ view/full_tour_request_post_view.php
@@ -1,3 +1,8 @@
+<?php session_start();
+//get the session value for varible
+$fullpost=$_SESSION['fullpost'];
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +10,7 @@
 	<title>Full Tour Request Post View-Ceylon Trek</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="../resources/css/tour_request_post.css">
+	<link rel="stylesheet" type="text/css" href="../resources/css/full_tour_request_view.css">
 	<link rel="stylesheet" href="../resources/css/top_bar.css">
     <link rel="stylesheet" href="../resources/css/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -13,22 +18,31 @@
 <body style="background-image: url('../resources/img/ct5.jpg'); background-size:cover;background-position: center center;background-attachment: fixed; background-repeat:no-repeat;">
 	<?php include('../view/top_bar.php'); ?>
 	<div class="full_tour_request_post_view_box">
-			<form action="full_tour_request_post_view.php" method="post">
+			<form action="..\controller\full_tour_request_post_view_controller.php" method="GET" >
 				
 				<div class="text_box">
 					<img src="../resources/img/ct6.jpg" width="112" height="112" style="border-radius: 65px;vertical-align:middle;float: left;">
-					<h1>Title</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					<br>
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<!--get associative array element to display-->
+						
+                        <!--get associative array element to display-->
+                        <?php
+					      foreach ($fullpost as $fullpost_element){
+						 ?>
+						 
+						 <ul>
+							<h3><?php echo $fullpost_element['title']; ?></h3>
+							<li><i class='fa fa-calendar' aria-hidden='true'></i>Requested Date :<?php echo $fullpost_element['requested_date']; ?></li>
+							<li><i class='fa fa-clone' aria-hidden='true'></i>NO Of Dates :<?php echo $fullpost_element['no_of_days']; ?></li>
+							<li><i class='fa fa-map-marker' aria-hidden='true'  style="margin-right:25px"></i>Places :<?php echo $fullpost_element['places']; ?></li>
+							<li><i class='fa fa-futbol-o' aria-hidden='true'></i>Activities & Other Details :<?php echo $fullpost_element['activities']; ?></li>
+							
+						</ul>
+
+
+					    <?php
+						  }  
+					    ?> 	   
+
 				</div><!-- text_box -->
 				<button class="msg">Message Tourist</button>
 				<button class="report">Report</button>
