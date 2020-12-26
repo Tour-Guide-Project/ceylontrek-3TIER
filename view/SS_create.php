615223ead91634fe1b429f76a24b886309b03393
sulakshanee <sulakshanee111@gmail.com>
2020-12-27 00:13:23 +0530

    sula crate ss

---
+++ view/SS_create.php
@@ -0,0 +1,70 @@
+<?php session_start();?>
+<html  lang="en">
+<head>
+    <title>Create Smart Search Place</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='../resources/css/Guidedashboardpage.css'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../resources/css/top_bar.css">
+    <link rel="stylesheet" href="../resources/css/footer.css">
+    <link rel="stylesheet" type="text/css" href="../resources/css/SS_create.css">
+</head>
+
+<body class="body">
+    <div class="section1"> 
+        <?php 
+            if (!isset($_SESSION['id'])){
+                include('../view/top_bar.php');
+            }
+            else{
+                include('../view/new_top_bar.php');
+            }
+        ?> 
+    
+        <div class="content">
+            <div class="con">
+                <form  action="../controller/createTourPackage_controller.php"  method="post" enctype="multipart/form-data" >
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="place_name" class="lbl">Name Of Place :</label>
+                        </div>
+                        <div class="col-75">
+                            <input type="text" name="place_name">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="short_description" class="lbl">Short Description about Place :</label>
+                        </div>
+                        <div class="col-75">
+                            <textarea name="short_description" style="height: 100px"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="long_description" class="lbl">Long Description about Place :</label>
+                        </div>
+                        <div class="col-75">
+                            <textarea name="long_description" style="height: 200px"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="image_upload" class="lbl">Upload Image :</label>
+                        </div>
+                        <div class="col-75">
+                            <input type="file"   name="file[0]" >
+                        </div>
+                    </div>
+                    <div class="submitCls">
+                        <!-- <input type="submit" name="createPackage" value="Create Package"> -->
+                        <button class="btnbtn" id="btn">Create Place</button>
+                    </div>
+                </form>
+            </div>
+            <!-- form -->
+        </div>
+        <div class="dashend"> <?php include('../view/footer.php'); ?> </div>
+
+    </div>
+</body>
+</html>
