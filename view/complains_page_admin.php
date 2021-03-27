eafe356cce68feff0cba5d1f012e4e9e07ed83dd
KAVINDYADEVINDI <63293156+KAVINDYADEVINDI@users.noreply.github.com>
2021-03-27 10:08:23 +0530

    kavi finalized admin and mod

--- view/complains_page_admin.php
+++ view/complains_page_admin.php
@@ -24,7 +24,7 @@
 	<div class="side_bar">
         <img src="../resources/img/logo2.png" class="dashlogo">
         <img src="../resources/img/reviewimg.jpg" class="profile" >
-		<form action="../controller/moderator_dashboard_controller.php" method="post">
+		<form action="../controller/admin_dashboard_controller.php" method="post">
         	<button class="edit" name="edit_profile" type="submit"> Edit Profile</button><br>
 		</form>
 		<div class="sidebar-menu">
@@ -36,7 +36,7 @@
 					</a>
 				</li>
 				<li>
-					<a href="inbox.php">
+					<a href="../controller/chat_controller.php">
 						<span class="menu-icon"><i class="fa fa-comment-o" aria-hidden="true"></i></span>
 						<span class="menu-title">Inbox</span>
 					</a>
