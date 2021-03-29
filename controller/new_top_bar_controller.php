c5a708fae036287150ea683fe316dfab8fe69104
Dinendra Karunarathne <dinendradb@gmail.com>
2021-03-30 00:14:16 +0530

    dinendra

--- controller/new_top_bar_controller.php
+++ controller/new_top_bar_controller.php
@@ -3,7 +3,7 @@
 $level=$_SESSION['level'];
  
 if ($level=='tourist'){
-    header('Location:/ceylontrek-3tier/view/touristDashboard.php');
+    header('Location:/ceylontrek-3tier/controller/view_tourist_dashboard_controller.php');
 }
 if ($level=='tourguide'){
     header('Location:/ceylontrek-3tier/controller/view_guide_dashboard_controller.php');
